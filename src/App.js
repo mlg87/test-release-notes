002036c9d3a6c9746cecce462ff656bf16d1c552
Mason Goetz <masonlgoetz@gmail.com>
2019-10-24 13:31:34 -0600

    Change it back to something else

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hi blake, how are you
+          Nah
         </a>
       </header>
     </div>
