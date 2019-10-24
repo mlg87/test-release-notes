67a41189bfcb5111bc14ceed44be3ae27dea6835
Mason Goetz <masonlgoetz@gmail.com>
2019-10-24 13:33:13 -0600

    This will be squashed and is an easy task (#5)

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Nah
+          this will be squashed
         </a>
       </header>
     </div>
